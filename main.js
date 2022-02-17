db266998fdec69b06807878a662c961da9f83ca3
Guilherme Cardoso <86062682+gccardosin@users.noreply.github.com>
2022-02-17 14:48:04 -0300

    Add files via upload

--- main.js
+++ main.js
@@ -1,6 +1,33 @@
 // DOM Document Object Model
 
+/**Toggle Menu, abre e fecha o menu ao clicar no icone */
 const nav = document.querySelector('#header nav')
 const toggle = document.querySelectorAll('nav .toggle')
 
-//https://youtu.be/9joIiCpvkG0?t=4535
+//adiciona o show ou tira quando necessario
+for (const div of toggle) {
+  div.addEventListener('click', function() {
+    nav.classList.toggle('show')
+  })
+}
+
+/**quando clicar em um item o menu fecha */
+const links = document.querySelectorAll('nav ul li a')
+
+for (const link of links) {
+  link.addEventListener('click', function() {
+    nav.classList.remove('show')
+  })
+}
+
+/**mudar o header da pagina quando scroll ¨Sombra¨ */
+const header = document.querySelector('#header')
+const navHeight = header.offsetHeight
+
+window.addEventListener('scroll', function() {
+  if (window.scrollY >= navHeight) {
+    header.classList.add('scroll')
+  } else {
+    header.classList.remove('scroll')
+  }
+})
